7e6a33c212e9067dbaf0fb40ae030212ef52bcbc
Wahyu Rahmaniar <wahyurahmaniar@yahoo.com>
2019-07-12 22:22:23 +0800

    Add files via upload
    Image Stabilization using Optical Flow and Kalman Filter.
    I use OpenCV 3.2.0.

---
+++ stabilization.cpp
@@ -0,0 +1,242 @@
+﻿
+#include "stdafx.h"
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <cassert>
+#include <cmath>
+#include <fstream>
+#include "opencv2/core.hpp"
+#include "opencv2/core/utility.hpp"
+#include "opencv2/video.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/videostab.hpp"
+#include "opencv2/opencv_modules.hpp"
+
+using namespace cv;
+using namespace std;
+
+
+// This video stablisation smooths the global trajectory using a sliding average window
+
+const int HORIZONTAL_BORDER_CROP = 50; 
+// Crops the border to reduce the black borders from stabilisation being too noticeable.
+
+struct TransformParam
+{
+    TransformParam() {}
+    TransformParam(double _dx, double _dy, double _da) {
+        dx = _dx;
+        dy = _dy;
+        da = _da;
+    }
+
+    double dx;
+    double dy;
+    double da; // angle
+};
+
+struct Trajectory
+{
+    Trajectory() {}
+    Trajectory(double _x, double _y, double _a) {
+        x = _x;
+        y = _y;
+        a = _a;
+    }
+	
+	friend Trajectory operator+(const Trajectory &c1,const Trajectory  &c2){
+		return Trajectory(c1.x+c2.x,c1.y+c2.y,c1.a+c2.a);
+	}
+
+	friend Trajectory operator-(const Trajectory &c1,const Trajectory  &c2){
+		return Trajectory(c1.x-c2.x,c1.y-c2.y,c1.a-c2.a);
+	}
+
+	friend Trajectory operator*(const Trajectory &c1,const Trajectory  &c2){
+		return Trajectory(c1.x*c2.x,c1.y*c2.y,c1.a*c2.a);
+	}
+
+	friend Trajectory operator/(const Trajectory &c1,const Trajectory  &c2){
+		return Trajectory(c1.x/c2.x,c1.y/c2.y,c1.a/c2.a);
+	}
+
+	Trajectory operator =(const Trajectory &rx){
+		x = rx.x;
+		y = rx.y;
+		a = rx.a;
+		return Trajectory(x,y,a);
+	}
+
+    double x;
+    double y;
+    double a; // angle
+};
+//
+int main(int argc, char **argv)
+{
+	const string source = "video.avi";
+
+	VideoCapture cap(source);
+
+	Mat cur, cur2, cur_grey;
+	Mat prev, prev_grey;
+
+	cap >> prev;//get the first frame.ch
+	cvtColor(prev, prev_grey, COLOR_BGR2GRAY);
+	
+	// Step 1 - Get previous to current frame transformation (dx, dy, da) for all frames
+	vector <TransformParam> prev_to_cur_transform; // previous to current
+	// Accumulated frame to frame transform
+	double a = 0;
+	double x = 0;
+	double y = 0;
+	// Step 2 - Accumulate the transformations to get the image trajectory
+	vector <Trajectory> trajectory; // trajectory at all frames
+	//
+	// Step 3 - Smooth out the trajectory using an averaging window
+	vector <Trajectory> smoothed_trajectory; // trajectory at all frames
+	Trajectory X;//posteriori state estimate
+	Trajectory	X_;//priori estimate
+	Trajectory P;// posteriori estimate error covariance
+	Trajectory P_;// priori estimate error covariance
+	Trajectory K;//gain
+	Trajectory	z;//actual measurement
+	double pstd = 4e-3;//can be changed
+	double cstd = 0.25;//can be changed
+	Trajectory Q(pstd,pstd,pstd);// process noise covariance
+	Trajectory R(cstd,cstd,cstd);// measurement noise covariance 
+	// Step 4 - Generate new set of previous to current transform, such that the trajectory ends up being the same as the smoothed trajectory
+	vector <TransformParam> new_prev_to_cur_transform;
+	//
+	// Step 5 - Apply the new transformation to the video
+	Mat T(2,3,CV_64F);
+
+	int ex = static_cast<int>(cap.get(CV_CAP_PROP_FOURCC));  
+
+	int vert_border = HORIZONTAL_BORDER_CROP * prev.rows / prev.cols; // get the aspect ratio correct
+	VideoWriter outputVideo; 
+	outputVideo.open("result_stab.avi" , CV_FOURCC('X','V','I','D'), 20, cvSize(cur.rows, cur.cols), true);  
+
+	int k=1;
+	
+	Mat last_T;
+	Mat prev_grey_,cur_grey_;
+	
+	double t1 = (double)getTickCount();
+	while(true) {
+
+		cap >> cur;
+		if(cur.data == NULL) {
+			break;
+		}		
+		cvtColor(cur, cur_grey, COLOR_BGR2GRAY);
+
+		// vector from prev to cur
+		vector <Point2f> prev_corner, cur_corner;
+		vector <Point2f> prev_corner2, cur_corner2;
+		vector <uchar> status;
+		vector <float> err;
+				
+		goodFeaturesToTrack(prev_grey, prev_corner, 100, 0.05, 30);
+		calcOpticalFlowPyrLK(prev_grey, cur_grey, prev_corner, cur_corner, status, err);
+
+		// weed out bad matches
+		for(int i=0; i < status.size(); i++) {
+			if(status[i]) {
+				prev_corner2.push_back(prev_corner[i]);
+				cur_corner2.push_back(cur_corner[i]);
+			}
+		}
+
+		// translation + rotation only
+		Mat T = estimateRigidTransform(prev_corner2, cur_corner2, true); // false = rigid transform, no scaling/shearing
+
+		// in rare cases no transform is found
+		if(T.data == NULL) {
+			last_T.copyTo(T);
+		}
+
+		T.copyTo(last_T);
+
+		// decompose T
+		double dx = T.at<double>(0,2);
+		double dy = T.at<double>(1,2);
+		double da = atan2(T.at<double>(1,0), T.at<double>(0,0));
+		
+		// Accumulated frame to frame transform
+		x += dx;
+		y += dy;
+		a += da;
+		z = Trajectory(x,y,a);
+				
+		if(k==1)
+		{
+			// intial guesses
+			X = Trajectory(0,0,0); //Initial estimate,  set 0
+			P =Trajectory(1,1,1); //set error variance,set 1
+		}
+		else
+		{
+			//time updateprediction
+			X_ = X; //X_(k) = X(k-1);
+			P_ = P+Q; //P_(k) = P(k-1)+Q;
+			// measurement update correction
+			K = P_/( P_+R ); //gain;K(k) = P_(k)/( P_(k)+R );
+			X = X_+K*(z-X_); //z-X_ is residual,X(k) = X_(k)+K(k)*(z(k)-X_(k)); 
+			P = (Trajectory(1,1,1)-K)*P_; //P(k) = (1-K(k))*P_(k);
+		}
+
+		// target - current
+		double diff_x = X.x - x;//
+		double diff_y = X.y - y;
+		double diff_a = X.a - a;
+
+		dx = dx + diff_x;
+		dy = dy + diff_y;
+		da = da + diff_a;
+
+		T.at<double>(0,0) = cos(da);
+		T.at<double>(0,1) = -sin(da);
+		T.at<double>(1,0) = sin(da);
+		T.at<double>(1,1) = cos(da);
+
+		T.at<double>(0,2) = dx;
+		T.at<double>(1,2) = dy;
+
+		
+		warpAffine(prev, cur2, T, cur.size());
+		
+		cur2 = cur2(Range(vert_border, cur2.rows-vert_border), Range(HORIZONTAL_BORDER_CROP, cur2.cols-HORIZONTAL_BORDER_CROP));
+
+		// Resize cur2 back to cur size, for better side by side comparison
+		resize(cur2, cur2, cur.size());
+		putText(prev, "before", Point(150, 20), 1, 2, Scalar(255, 255, 255), 2, 8, 0);
+		putText(cur2, "after", Point(150, 20), 1, 2, Scalar(255, 255, 255), 2, 8, 0);
+
+		// Now draw the original and stablised side by side 
+		Mat canvas = Mat::zeros(cur.rows, cur.cols*2+3, cur.type());
+
+		prev.copyTo(canvas(Range::all(), Range(0, cur2.cols)));
+		cur2.copyTo(canvas(Range::all(), Range(cur2.cols+3, cur2.cols*2+3)));
+
+		if(canvas.cols > 1920) 
+		{
+			resize(canvas, canvas, Size(canvas.cols/2, canvas.rows/2));
+		}
+		
+
+		imshow("Video Stabilization Result", canvas);
+
+
+		prev = cur.clone();
+		cur_grey.copyTo(prev_grey);
+	
+		k++;
+		waitKey(1);
+				
+    }
+
+	return 0;
+}
